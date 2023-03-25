e0613df53863abe47e69a47c3847bc104edace59
Svetlana <khabarova09@rambler.ru>
2023-03-25 13:54:02 +0300

    сколько чётных чисел в массиве

---
+++ ArDif3digNum_CountEven/Program.cs
@@ -0,0 +1,34 @@
+﻿// Массив заполнить случайными трехзначными числами
+// Сколько чётных чисел в массиве? [345,897,568,234]->2
+
+void FillArray(int[] arr)
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] = new Random().Next(100, 1000);
+  }
+}
+
+void PrintArray(int[] arr)
+{
+  foreach (int i in arr)
+  {
+    Console.Write($"{i},");
+  }
+  Console.WriteLine();
+}
+
+int CountEven(int[] ev)
+{
+  int count = 0;
+  foreach (int i in ev)
+    if (i % 2 == 0)
+      count++;
+  return count;
+}
+
+int[] array = new int[5];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(CountEven(array));
+
